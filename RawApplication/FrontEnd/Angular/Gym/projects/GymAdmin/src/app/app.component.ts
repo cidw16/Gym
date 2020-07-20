a2de0658f05efe94f9d4726c49f9ff1ffdc46cc3
Roberto <roberto.delcid@gmail.com>
2020-07-19 23:26:23 -0600

    Inital upload

---
+++ RawApplication/FrontEnd/Angular/Gym/projects/GymAdmin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gym-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'GymAdmin';
+}
